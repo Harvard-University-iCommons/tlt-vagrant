f71b9bf4f945a32d359b0bedcd83d87b3364f650
Douglas Hall <douglas_hall@harvard.edu>
2015-03-18 21:11:47 -0400

    Fixed requirements for the django apps

--- vagrant/venv_bootstrap.sh
+++ vagrant/venv_bootstrap.sh
@@ -6,11 +6,11 @@ source /usr/local/bin/virtualenvwrapper.sh
 # Make icommons_lti_tools virtualenv
 mkvirtualenv -a /home/vagrant/icommons_lti_tools icommons_lti_tools
 workon icommons_lti_tools
-#pip install -r /home/vagrant/icommons_lti_tools/icommons_lti_tools/requirements/base.txt
-pip install -r /vagrant/requirements/icommons_lti_tools.txt
+pip install -r /home/vagrant/icommons_lti_tools/icommons_lti_tools/requirements/base.txt
+pip install -r /vagrant/vagrant/requirements/icommons_lti_tools.txt
 
 # Make ab-testing-tool virtualenv
 mkvirtualenv -a /home/vagrant/ab-testing-tool ab-testing-tool
 workon ab-testing-tool
-#pip install -r /home/vagrant/ab-testing-tool/ab-testing-tool/requirements/base.txt
-#pip install -r requirements/ab-testing-tool.txt
+pip install -r /home/vagrant/ab-testing-tool/ab_testing_tool/requirements/base.txt
+pip install -r /vagrant/vagrant/requirements/ab_testing_tool.txt
